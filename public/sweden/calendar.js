fec630c0cc87ad4819fc9dac8d8d3ee333b68c83
AntoineMdn <antoine.mandin.am@gmail.com>
2021-09-09 09:13:32 +0200

    Clean code

--- public/sweden/calendar.js
+++ public/sweden/calendar.js
@@ -1,42 +1,4 @@
 (() => {
-    const buildEventElement = event => {
-        const _c = (name, parent = undefined) => {
-            let res = document.createElement(name)
-            if (parent)
-                parent.appendChild(res)
-            return res
-        }
-        let container = _c("div")
-        container.classList.add("event")
-        container.classList.add("container")
-
-        let title = _c("h2", container)
-        title.innerText = event.summary
-
-        let list = _c("ul", container)
-        if (event.type) {
-            let itemType = _c("li", list)
-            itemType.innerText = event.type
-        }
-
-        if(event.location) {
-            let text = event.location
-            if(event.location2 && event.location2 != event.location){
-                text += " ("+event.location2+")"
-            }
-            let itemLocation = _c("li", list)
-            if(event.locationUrl){
-                let linkLocation = _c("a", itemLocation)
-                linkLocation.href = event.locationUrl
-                linkLocation.innerText = text
-            } else {
-                itemLocation.innerText = text
-            }
-        }
-
-        return container
-    }
-
     const _qs = query => {
         return document.querySelector(query)
     }
@@ -57,7 +19,7 @@
 
         console.log("Calendar: ",calendar)
 
-        let stats = CalendarBuilder.setEvents3(cal.events)
+        let stats = CalendarBuilder.setEvents(cal.events)
         _qs("#btn-reload").disabled = false
 
         setProgress(stats.done, stats.total)
@@ -69,5 +31,6 @@
         event.target.disabled = true
     }
 
+    // Initialize Mdn library
     Mdn.init()
 })()
